7f03211d817629151e33327fc4cd0f65751cb7cd
IgnacioSens <ignacioa785@gmail.com>
2024-10-17 23:20:08 -0300

    tarefajs

--- aulajs/exercicio10.js
+++ aulajs/exercicio10.js
@@ -1,15 +1,10 @@
-let teste1="arara",teste2="birdou",teste3="ovo",teste4="ave",teste5="renner";
-function verificador(x)
+let teste1="arara",teste2="birdou",teste3="ovo",teste4="ave",teste5="RennEr",teste6="ReInIer";
+
+function verificador(m)
 {
+    let x=m.toLowerCase();
     let y=(x.length)-1, z=x.split(""),temp,cont=0;
     let b=y;
-    for(let a=0;a<y;a++,y--)
-    {
-        temp=z[a];
-        z[a]=z[y];
-        z[y]=temp;
-    }
-    z=z.join('');
     if(b%2==0)
     {
         for(a=0;a<b;a++,b--)
@@ -17,27 +12,33 @@ function verificador(x)
                 if(z[a]==z[b])
                     cont++;
             }
+            
         if(cont==b)
             return "um palidromo"
     }
     else
     {
-        b=b-1;
+
+        
     for(a=0;a<b;a++,b--)
         {
             if(z[a]==z[b])
                 cont++;
         }
+        
     }
-    if(cont==b/2) 
+    b++;
+    if(cont==b) 
         {
-        return "um palidromo",z
+              
+        return "um palidromo"
         }
     else
     return "Não é um palindromo"
 }
-console.log(verificador(teste1))
-console.log(verificador(teste2))
-console.log(verificador(teste3))
-console.log(verificador(teste4))
-console.log(verificador(teste5))
+console.log("1.",verificador(teste1))
+console.log("2.",verificador(teste2))
+console.log("3.",verificador(teste3))
+console.log("4.",verificador(teste4))
+console.log("5.",verificador(teste5))
+console.log("6.",verificador(teste6))
